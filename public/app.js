23ba2b51023cf081ccea91cd86f5feb158f76e0e
Azhar <azhar40@live.co.uk>
2021-01-13 17:53:20 +0500

    aa

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
-const url = 'https://loginjwtmongo.herokuapp.com';
-// const url = "http://localhost:7000";
+// const url = 'https://loginjwtmongo.herokuapp.com';
+const url = "http://localhost:7000";
 
 
 
